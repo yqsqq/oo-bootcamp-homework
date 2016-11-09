b4749d6c0fb2aa2f7f291de79b7d9c43315a40dd
Zhang Xin <xinzhang@xinzhangs-MacBook-Pro.local>
2016-11-09 10:25:55 +0800

    add unit test

--- src/java/parse/FormatPostcode.java
+++ src/java/parse/FormatPostcode.java
@@ -12,18 +12,18 @@ public class FormatPostcode {
 
     public String input(String postcode) throws Exception{
         String newPostcode = postcode.replace("-", "");
-        if (!IsLengthValid(postcode)) {
+        if (!IsLengthValid(newPostcode)) {
             throw new Exception("the length of post code is invalid");
         }
         if (!IsContentValid(newPostcode)) {
             throw new Exception("the content of post code is invalid");
         }
-        return newPostcode;
+        return newPostcode + calcCheckCode(newPostcode);
     }
 
     public String output(String postcode) {
         if (postcode.length() == 9) {
-            return postcode.substring(0, 5) + "-" + postcode.substring(6);
+            return postcode.substring(0, 5) + "-" + postcode.substring(5);
         }
         return postcode;
     }
@@ -39,5 +39,13 @@ public class FormatPostcode {
 
     private boolean IsContentValid(String postcode) {
         return Pattern.matches("[0-9]*", postcode);
+    }
+
+    private int calcCheckCode (String postcode) {
+        int sum = 0;
+        for (int i = 0; i < postcode.length(); i++) {
+            sum += postcode.charAt(i) - '0';
+        }
+        return sum%10 == 0 ? 0 : 10-sum%10;
     }
 }
--- src/test/ConvertorTest.java
+++ src/test/ConvertorTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -6,11 +7,41 @@ import static org.junit.Assert.*;
  */
 public class ConvertorTest {
 
+    private Convertor convertor;
+
+    @Before
+    public void setUp() {
+        convertor = new Convertor();
+    }
+
     @Test
-    public void shouldReturnCorrectPostcode() throws Exception {
-        Convertor convertor = new Convertor();
+    public void shouldReturn5DigitPostcode() throws Exception {
         String postcode = convertor.barcode2postcode("| |:|:: :|:|: |:::| :::|| ::||: :|:|: |");
         assertEquals("95713", postcode);
+    }
+
+    @Test
+    public void shouldReturn5DigitBarcode() throws Exception {
+        String barcode = convertor.postcode2barcode("95713");
+        assertEquals("| |:|:: :|:|: |:::| :::|| ::||: :|:|: |", barcode);
+    }
+
+    @Test
+    public void shouldReturn10DigitPostcode() throws Exception {
+        String barcode = convertor.barcode2postcode("| |:|:: :|:|: |:::| :::|| ::||: :::|| ::|:| ::||: :|::| :|:|: |");
+        assertEquals("95713-1234", barcode);
+    }
+
+    @Test
+    public void shouldConvert9DigitPostcode29DigitBarcode() throws Exception {
+        String barcode = convertor.postcode2barcode("957131234");
+        assertEquals("| |:|:: :|:|: |:::| :::|| ::||: :::|| ::|:| ::||: :|::| :|:|: |", barcode);
+    }
+
+    @Test
+    public void shouldConvert10DigitPostcode29DigitBarcode() throws Exception {
+        String barcode = convertor.postcode2barcode("95713-1234");
+        assertEquals("| |:|:: :|:|: |:::| :::|| ::||: :::|| ::|:| ::||: :|::| :|:|: |", barcode);
     }
 
 }
